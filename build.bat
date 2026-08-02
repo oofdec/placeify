b19a7e1f5b4f3bb113648790d4239bd9601b046b
oofdec <127800938+oofdec@users.noreply.github.com>
2026-08-02 13:56:21 -0400

    Update build.bat
    i dont know how to code in bash

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 pyinstaller --onefile --noconsole ^
   --add-data "assets;." ^
-  --icon="assets/placeify.ico" ^
+  --icon="assets/placeify.ico;." ^
   --hidden-import=yt_dlp ^
   --hidden-import=yt_dlp.extractor ^
   --hidden-import=yt_dlp.postprocessor ^
